b4b63446844a486a92153c7ab4579b938525da6c
upendrashrestha10 <shresthaupendra1234@gmail.com>
2025-07-29 16:09:21 +0545

    variables

---
+++ Day1/variables.dart
@@ -0,0 +1,8 @@
+void main(){
+  var Ram = 10;
+    //Here the value "10" stores in "Ram" which is variable. 
+    //And here we can see "=" sign which is assign operater which assign the value '10' in variable "Ram".
+  var Sam = 20;
+  var sum = Ram+Sam;
+  print(sum);
+}
