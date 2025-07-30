7affe401d0c793e51cc52d1a771ca7154e088682
upendrashrestha10 <shresthaupendra1234@gmail.com>
2025-07-30 17:20:15 +0545

    collection

---
+++ Day2/Collection/collection.dart
@@ -0,0 +1,10 @@
+/*
+  
+Collections are used to **store multiple values** in one place. Dart provides:
+
+1. **List** – Like arrays (ordered)
+2. **Set** – Unique values only (unordered)
+3. **Map** – Key-value pairs
+
+  To know more about collection you can see notes which is on readme.md file.
+*/
