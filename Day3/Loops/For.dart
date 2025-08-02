4e2887678dc777c2bc6b29cb459e93c47d2aa761
upendrashrestha10 <shresthaupendra1234@gmail.com>
2025-08-02 16:28:42 +0545

    For loop

---
+++ Day3/Loops/For.dart
@@ -0,0 +1,22 @@
+/**
+   A for loop is used when you want to run a block of code a specific number of times. 
+   It consists of an initializer, condition, and increment/decrement in a single line.
+
+  🔹 Syntax:
+      for (initialization, condition, increment/decrement){
+      ....
+      .....
+      }
+      for (int i = 0; i < 5; i++) {
+        // code to repeat
+      }
+  This example runs the loop 5 times, printing or processing something each time.
+
+  For more information, you can see my detailed notes which is on README.md file.
+*/
+
+void main(){
+  for(int i = 0; i<=10; i++){
+    print (i);
+  }
+}
